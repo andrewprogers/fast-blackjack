fc142bbbfa35fb705ed38dbf19ac4289cf9c9e03
Andrew Rogers <andrewprogers@gmail.com>
2017-06-02 12:32:46 -0400

    Added actions for players turn

--- blackjack.rb
+++ blackjack.rb
@@ -35,7 +35,7 @@ end
 class Dealer
   def initialize(num_decks)
     @dealer_cards = []
-    @dealer_secret_cards = []
+    @dealer_secret_card = nil
     @deck = []
     ranks = [*(1..13)]
     suits = "Clubs Diamonds Hearts Spades".split(" ")
@@ -61,20 +61,28 @@ class Dealer
   end
 
   def add_secret_card(card)
-    @dealer_secret_cards << card
+    @dealer_secret_card = card
+  end
+
+  def list_hand
+    puts "\nCards in the dealers hand:"
+    @dealer_cards.each { |card| puts "  " + card.name }
+    puts "  ...and one face-down card" unless @dealer_secret_card.nil?
   end
 
 end
 
 class Player
+  attr_accessor :stay
   def initialize()
     @cards = []
+    @stay = false
   end
 
   def total
     sum = 0
-    cards.each do |card|
-      sum += card.rank
+    @cards.each do |card|
+      sum += card.value
     end
     sum
   end
@@ -88,20 +96,43 @@ class Player
   end
 
   def list_hand
-    puts "Cards in the players hand:"
-    @cards.each { |card| puts card.name }
+    puts "\nCards in the players hand:"
+    @cards.each { |card| puts "  " + card.name }
   end
 end
 
 puts "Welcome to Blackjack."
 
 #game init
-puts "Starting a new game"
 player = Player.new()
 dealer = Dealer.new(1)
+dealer.shuffle()
 
 puts "The dealer deals you your hand."
 player.hit(dealer.deal)
 dealer.add_card(dealer.deal)
 player.hit(dealer.deal)
 dealer.add_secret_card(dealer.deal)
+
+while ( !(player.stay || player.bust?) ) do
+  player.list_hand
+  dealer.list_hand
+
+  print "\nHit - h, Stay - s (or any other key): "
+  char = gets.chomp
+  if char == 'h'
+    card = dealer.deal
+    puts "The dealer deals you a #{card.name}"
+    player.hit(card)
+  else
+    puts "You stay"
+    player.stay = true
+  end
+end
+
+if (player.bust?)
+  puts "You've gone bust!!! You lose"
+  return
+else
+  puts "It is the dealers turn"
+end

b6c36b16dbb002f7df7012d9bf6edd5b80c021be
Andrew Rogers <andrewprogers@gmail.com>
2017-06-03 08:32:17 -0400

    Bundle Game into a class

--- blackjack.rb
+++ blackjack.rb
@@ -26,7 +26,6 @@ class Card
     else
       @rank
     end
-
     "#{face} of #{@suit}"
   end
 end
@@ -106,81 +105,84 @@ class Player < GenericHand
     super
     @name = "Player"
   end
-
 end
 
-def print_title
-  puts "Welcome to Blackjack."
-  puts "----------------------------\n\n"
-end
-
-def deal_initial(player, dealer, deck)
-  puts "The dealer deals you your hand."
-  player.hit(deck.deal)
-  dealer.hit(deck.deal)
-  player.hit(deck.deal)
-  dealer.deal_facedown(deck.deal)
-  puts "\n"
-end
-
-def show_hands(*hands)
-  hands.each { |hand| hand.list_hand }
-end
-
-def player_turn(player, dealer, deck)
-  while ( !player.stay ) do
-    show_hands(player, dealer)
-
-    print "\nHit - h, Stay - s (or any other key): "
-    char = gets.chomp
-    if char == 'h'
-      card = deck.deal
-      player.hit(card)
-      break if player.bust
-    else
-      puts "You stay"
-      player.stay = true
-    end
+class Game
+  attr_reader :player, :dealer, :deck
+  def initialize()
+    @player = Player.new()
+    @dealer = Dealer.new()
+    @deck = Deck.new()
   end
-end
 
-def dealer_turn(dealer, deck)
-  puts "It is the dealers turn"
-  dealer.reveal_card
-  while dealer.total <= 16 do
+  def deal_initial(player, dealer, deck)
+    puts "The dealer deals you your hand."
+    player.hit(deck.deal)
     dealer.hit(deck.deal)
+    player.hit(deck.deal)
+    dealer.deal_facedown(deck.deal)
+    puts "\n"
   end
-end
 
-def scoring(player, dealer)
-  puts "Final Hands:"
-  show_hands(player, dealer)
-  if player.bust
-    puts "You went bust, dealer wins"
-  elsif dealer.bust
-    puts "Dealer goes bust, player wins"
-  else
-    p_score = player.total
-    d_score = dealer.total
-    puts "Player: #{p_score}. Dealer: #{d_score}."
-    if p_score > d_score
-      puts "Player wins"
-    elsif d_score > p_score
-      puts "Dealer wins"
-    else
-      puts "Tie game"
+  def show_hands(*hands)
+    hands.each { |hand| hand.list_hand }
+  end
+
+  def player_turn(player, dealer, deck)
+    while ( !player.stay ) do
+      show_hands(player, dealer)
+
+      print "\nHit - h, Stay - s (or any other key): "
+      char = gets.chomp
+      if char == 'h'
+        card = deck.deal
+        player.hit(card)
+        break if player.bust
+      else
+        puts "You stay"
+        player.stay = true
+      end
     end
   end
+
+  def dealer_turn(dealer, deck)
+    puts "It is the dealers turn"
+    dealer.reveal_card
+    while dealer.total <= 16 do
+      dealer.hit(deck.deal)
+    end
+  end
+
+  def scoring(player, dealer)
+    puts "Final Hands:"
+    show_hands(player, dealer)
+    if player.bust
+      puts "You went bust, dealer wins"
+    elsif dealer.bust
+      puts "Dealer goes bust, player wins"
+    else
+      p_score = player.total
+      d_score = dealer.total
+      puts "Player: #{p_score}. Dealer: #{d_score}."
+      if p_score > d_score
+        puts "Player wins"
+      elsif d_score > p_score
+        puts "Dealer wins"
+      else
+        puts "Tie game"
+      end
+    end
+  end
+
+  def play
+    deal_initial(@player, @dealer, @deck)
+    player_turn(@player, @dealer, @deck)
+    dealer_turn(@dealer, @deck) unless @player.bust
+    scoring(@player, @dealer)
+  end
 end
 
 
-print_title
-
-player = Player.new()
-dealer = Dealer.new()
-deck = Deck.new()
-
-deal_initial(player, dealer, deck)
-player_turn(player, dealer, deck)
-dealer_turn(dealer, deck) unless player.bust
-scoring(player, dealer)
+puts "Welcome to Blackjack."
+puts "----------------------------\n\n"
+Game.new().play()
